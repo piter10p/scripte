5090773b1366f7aa31c7096d48576156c9afbcde
Piotr Paszko <Piotr.Paszko@finture.com>
2019-10-17 15:10:09 +0200

    Update KeyEventConsumerContext.

--- scripte/src/app/services/document-body/KeyEventConsumers/KeyEventConsumerContext.ts
+++ scripte/src/app/services/document-body/KeyEventConsumers/KeyEventConsumerContext.ts
@@ -1,10 +1,12 @@
 import { TextSelection } from '../TextSelection';
+import { DocumentBody } from '../DocumentBody';
+import { DocumentPosition } from '../DocumentPosition';
 
 export class KeyEventConsumerContext {
 
     public event: KeyboardEvent;
-    public documentBody: string[];
-    public cursorPosition: number;
+    public documentBody: DocumentBody;
+    public cursorPosition: DocumentPosition;
     public selection: TextSelection;
 
     public documentBodyUpdateCallback: (context: KeyEventConsumerContext) => any;
